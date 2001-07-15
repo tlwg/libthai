d28749e2bb508729668596480594cf20b53f3630
ott <ott@07894a72-abb6-46fe-b0a1-707444d65a2c>
2001-07-15 16:35:24 +0000

    -add th_brk_line interface

--- trunk/include/thai/thbrk.h
+++ trunk/include/thai/thbrk.h
@@ -1,5 +1,5 @@
 /*
- * $Id: thbrk.h,v 1.1 2001-05-17 18:04:35 thep Exp $
+ * $Id: thbrk.h,v 1.2 2001-07-15 16:35:24 ott Exp $
  * thbrk.h - Thai word segmentation
  * Created: 2001-05-17
  */
@@ -13,11 +13,30 @@ BEGIN_CDECL
 
 /*
  * Find word break positions in Thai string s
+ *
+ * @param s : the input string to be processed
+ * @param pos : array to keep breaking positions (see on return)
+ * @pram n : size of pos[]
+ *
  * On returns: at most n breaking positions stored in pos[],
  *             from left to right
- * Returns: the actual number of positions stored in pos[]
+ * Returns: the actual number of breaking positions occurred
+ *
  */
 extern int th_brk(const thchar_t *s, int pos[], size_t n);
+
+/*
+ * Find word break positions in Thai string in
+ *
+ * @param in : the input string to be processed
+ * @param out : the output string after processed
+ * @param n : the size of "out"
+ * @param cutcode : the word-break character (default = 254)
+ * On returns: at most n breaking positions stored in pos[],
+ *             from left to right
+ * Returns: the actual size of the processed string
+ */
+extern int th_brk_line(const thchar_t *in, thchar_t *out, size_t n, int _cutcode );
 
 END_CDECL
 
