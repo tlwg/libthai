b2211ca4ba6ed8297fb0d5e7810d400dffb5daa2
chanop <chanop@07894a72-abb6-46fe-b0a1-707444d65a2c>
2001-07-17 05:00:47 +0000

    Dynamically generate map.c and map.h in a build directory. Remove map.c and map.h from the repository.

--- trunk/src/thbrk/map.h
+++
@@ -1,9 +0,0 @@
-int numword = 19304;
-int mincol = 161;
-int maxcol = 241;
-int maxstate = 48113;
-extern unsigned short map[208598];
-extern unsigned char state[48113];
-extern unsigned char state_min[48113];
-extern unsigned char state_max[48113];
-extern int state_offset[48113];
