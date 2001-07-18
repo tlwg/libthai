7ebe3e745df40d6088eb2fdebfaa591999309dc6
ott <pattara@gmail.com>
2001-07-18 23:43:41 +0000

    -change to be non-interactive unless "-i" option is specified. -still segfault, not yet finished

--- src/thbrk/test_thbrk.c
+++ src/thbrk/test_thbrk.c
@@ -1,6 +1,6 @@
 /* Test driver for thbrk 
  *
- * $Id: test_thbrk.c,v 1.1 2001-07-15 16:31:54 ott Exp $
+ * $Id: test_thbrk.c,v 1.2 2001-07-18 23:43:41 ott Exp $
  */
 
 #define MAXLINELENGTH 1000
@@ -8,6 +8,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <thai/thbrk.h>
+
+// run with "-i" argument to get the interactive version
+// otherwise it will run the self test and exit
 
 int main (int argc, char* argv[]) {
   
@@ -18,30 +21,66 @@ int main (int argc, char* argv[]) {
   int pos[MAXLINELENGTH];
   int outputLength;
   int numCut, i;
+  int interactive = 0;
   
   str = _str;
   retval = _retval;
   fp = stdin;
 
-  while(!feof(fp)) {
-    printf("Please enter thai words/sentences: ");
-    retval = "";
-    retval=(unsigned char *) fgets((char *)str, MAXLINELENGTH-1,fp);
-    if(!feof(fp)) {
-      numCut = th_brk(str,pos,MAXLINELENGTH);
-      printf ("Total %d cut points.", numCut);
-      if ( numCut > 0 ) { 
-	printf("Cut points list: %d", pos[0]);
-	for ( i = 1 ; i < numCut ; i++ ) {
-	  printf(", %d", pos[i]);
-	}
-      }
-      printf("\n");
-      outputLength = th_brk_line(str,out,MAXLINELENGTH*2+1,254);
-      printf("Output string length is %d\n",outputLength);
-      printf("Output string is %s",out);
-      printf("**********************************************************\n");
+
+  if ( argc >= 2 ) {
+    if ( argv[1][0] == '-' && argv[1][1] == 'i' ) {
+      interactive = 1;
     }
   };
+  if ( interactive == 1) {
+    while(!feof(fp)) {
+      printf("Please enter thai words/sentences: ");
+      retval = "";
+      retval=(unsigned char *) fgets((char *)str, MAXLINELENGTH-1,fp);
+      if(!feof(fp)) {
+	numCut = th_brk(str,pos,MAXLINELENGTH);
+	printf ("Total %d cut points.", numCut);
+	if ( numCut > 0 ) { 
+	  printf("Cut points list: %d", pos[0]);
+	  for ( i = 1 ; i < numCut ; i++ ) {
+	    printf(", %d", pos[i]);
+	  }
+	}
+	printf("\n");
+	outputLength = th_brk_line(str,out,MAXLINELENGTH*2+1,254);
+	printf("Output string length is %d\n",outputLength);
+	printf("Output string is %s",out);
+	printf("**********************************************************\n");
+      }; // if
+    }; //while
+  } else {
+    str = "สวัสดีครับ นี่เป็นการทดสอบตัวเอง";
+    printf ("Testing with string: %s\n",str);
+    numCut = th_brk(str,pos,MAXLINELENGTH);
+    printf ("Total %d cut points.", numCut);
+    if ( numCut == 7 ) { 
+      printf("Correct! .. test th_brk() passed...\n");
+    } else {
+      printf("Error! .. test th_brk() failed...\n");
+      exit (-1);
+    }
+	
+    printf("Cut points list: %d", pos[0]);
+    for ( i = 1 ; i < numCut ; i++ ) {
+      printf(", %d", pos[i]);
+    }
+    printf("\n");
+    outputLength = th_brk_line(str,out,MAXLINELENGTH*2+1,254);
+    printf("Output string length is %d\n",outputLength);
+    if ( outputLength == 40 ) {
+      printf("Correct! .. test th_brk_line() passed...\n");
+    } else {
+      printf("Error! .. test th_brk_line() failed...\n");
+      exit (-1);
+    }
+    printf("Output string is %s",out);
+    printf("*** End of thbrk self test ******\n");
+  }
   return 0;
 }
