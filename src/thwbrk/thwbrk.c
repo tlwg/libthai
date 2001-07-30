2114ac52b85fcda25e5ac97326a101043ec3eff8
ott <pattara@gmail.com>
2001-07-30 11:40:47 +0000

    -thwbrk implementation

---
+++ src/thwbrk/thwbrk.c
@@ -0,0 +1,87 @@
+/* Thai Word Break Library
+ * based on cttex by Vuthichai A. (vuthi@[crtl.titech.ac.jp|linux.thai.net])
+
+ * Created 2001-07-15
+ * $Id: thwbrk.c,v 1.1 2001-07-30 11:40:47 ott Exp $
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <memory.h>
+#include <stdlib.h>
+
+#include <thai/thailib.h>
+#include <thai/thwchar.h>
+#include <thai/thbrk.h>
+
+
+int th_wbrk(const thwchar_t *s, int pos[], size_t n) {
+  thchar_t* tis620_0_string;
+  unsigned int inputLength, inputLengthReturned;
+  int returnValue;
+
+  inputLength = wcslen(s);
+
+  // convert to tis620-0 string
+  tis620_0_string = malloc(inputLength);
+  inputLengthReturned = th_uni2tis_line(s, tis620_0_string, inputLength);
+  if ( inputLength != inputLengthReturned) {
+    free(tis620_0_string);
+    tis620_0_string = malloc(inputLengthReturned);
+    th_uni2tis_line(s, tis620_0_string, inputLengthReturned);
+  };
+  
+  // Do word cut
+  returnValue = th_brk(tis620_0_string, pos, n);
+
+  // Deallocate memory
+  free(tis620_0_string);
+
+  return returnValue;
+}
+
+int th_wbrk_line(const thwchar_t *in, thwchar_t *_out, size_t n, const thwchar_t* _cutCode ) {
+  thchar_t *tis620_0_string, *tis620_0_out, *tis620_0_cutCode;
+
+  unsigned int inputLength, inputLengthReturned, cutCodeLength;
+  unsigned int tis620_0_outLength;
+  int returnValue;
+
+  inputLength = wcslen(in);
+
+  // convert to tis620-0 string
+  tis620_0_string = malloc(inputLength);
+  inputLengthReturned = th_uni2tis_line(in, tis620_0_string, inputLength);
+  if ( inputLength != inputLengthReturned) {
+    free(tis620_0_string);
+    tis620_0_string = malloc(inputLengthReturned);
+    th_uni2tis_line(in, tis620_0_string, inputLengthReturned);
+  };
+  
+  // check the length of cutCode
+  cutCodeLength = wcslen(_cutCode);
+
+  // convert cutcode to tis620_0;
+  tis620_0_cutCode = malloc( cutCodeLength);
+  th_uni2tis_line(_cutCode, tis620_0_cutCode, cutCodeLength);
+
+  // malloc for output cut string
+  tis620_0_outLength = ( 1 + cutCodeLength ) * strlen(tis620_0_string) + 1;
+  tis620_0_out = malloc( tis620_0_outLength);
+  
+  // Do word cut
+  th_brk_line(tis620_0_string, tis620_0_out, tis620_0_outLength, tis620_0_cutCode);
+  
+  // Convert back
+  returnValue = th_tis2uni_line(tis620_0_out, _out, n);
+
+  // Doallocate memory
+  free(tis620_0_out);
+  free(tis620_0_cutCode);
+  free(tis620_0_string);
+
+  return returnValue;
+}
+
+
+
