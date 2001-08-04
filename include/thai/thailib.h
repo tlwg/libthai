766a9a188022b54491e071b6f94ff9bd0a3bb260
thep <theppitak@gmail.com>
2001-08-04 15:46:21 +0000

    Resolve conflict.

--- include/thai/thailib.h
+++ include/thai/thailib.h
@@ -1,5 +1,5 @@
 /*
- * $Id: thailib.h,v 1.4 2001-08-04 15:32:39 ott Exp $
+ * $Id: thailib.h,v 1.5 2001-08-04 15:46:21 thep Exp $
  * thailib.h - general declarations for libthai
  * Created: 2001-05-17
  */
@@ -17,7 +17,7 @@
 # define  END_CDECL
 #endif /* __cplusplus */
 
-#define THCHAR_ERR  ((thchar_t) ~0)
+#define THCHAR_ERR  ((thchar_t)~0)
 typedef unsigned char thchar_t;
 
 #endif  /* THAI_THAILIB_H */
