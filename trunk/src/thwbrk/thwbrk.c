ef1a19123c390dda8208dc3330548fb6ee9cc1a0
ott <ott@07894a72-abb6-46fe-b0a1-707444d65a2c>
2001-08-04 14:45:50 +0000

    -change comments to C-style :(

--- trunk/src/thwbrk/thwbrk.c
+++ trunk/src/thwbrk/thwbrk.c
@@ -2,7 +2,7 @@
  * based on cttex by Vuthichai A. (vuthi@[crtl.titech.ac.jp|linux.thai.net])
 
  * Created 2001-07-15
- * $Id: thwbrk.c,v 1.3 2001-08-01 13:13:59 ott Exp $
+ * $Id: thwbrk.c,v 1.4 2001-08-04 14:45:50 ott Exp $
  */
 
 #include <stdio.h>
@@ -22,7 +22,7 @@ int th_wbrk(const thwchar_t *s, int pos[], size_t n) {
 
   inputLength = wcslen(s);
 
-  // convert to tis620-0 string
+  /* convert to tis620-0 string */
   tis620_0_string = malloc(inputLength+1);
   inputLengthReturned = th_uni2tis_line(s, tis620_0_string, inputLength+1);
   if ( inputLength != inputLengthReturned) {
@@ -31,10 +31,10 @@ int th_wbrk(const thwchar_t *s, int pos[], size_t n) {
     th_uni2tis_line(s, tis620_0_string, inputLengthReturned+1);
   };
   
-  // Do word cut
+  /* Do word cut */
   returnValue = th_brk(tis620_0_string, pos, n);
 
-  // Deallocate memory
+  /* Deallocate memory */
   free(tis620_0_string);
 
   return returnValue;
@@ -52,7 +52,7 @@ int th_wbrk_line(const thwchar_t *in, thwchar_t *_out, size_t n, const thwchar_t
 
   inputLength = wcslen(in);
 
-  // convert to tis620-0 string
+  /* convert to tis620-0 string */
   tis620_0_string = malloc(inputLength+1);
   inputLengthReturned = th_uni2tis_line(in, tis620_0_string, inputLength+1);
   if ( inputLength != inputLengthReturned) {
@@ -61,41 +61,41 @@ int th_wbrk_line(const thwchar_t *in, thwchar_t *_out, size_t n, const thwchar_t
     th_uni2tis_line(in, tis620_0_string, inputLengthReturned+1);
   };
   
-  // malloc for tis620-0output cut string
+  /* malloc for tis620-0output cut string */
   tis620_0_outLength =  2 * strlen(tis620_0_string);
   tis620_0_out = malloc( tis620_0_outLength+1);
   
-  // Do word cut
+  /* Do word cut */
   tis620_0_outLength = th_brk_line(tis620_0_string, tis620_0_out, tis620_0_outLength+1, cutCode);
   
-  // Create equivalent unicode output
+  /* Create equivalent unicode output */
   uni_out = (thwchar_t *) malloc((tis620_0_outLength+1)*sizeof(thwchar_t));
   th_tis2uni_line( tis620_0_out, uni_out, tis620_0_outLength+1);
   
-  // check the length of cutCode
+  /* check the length of cutCode */
   cutCodeLength = wcslen(_cutCode);
 
-  // malloc the pre output unicode string
+  /* malloc the pre output unicode string */
   preoutLength = tis620_0_outLength * cutCodeLength;
   preout = (thwchar_t *) malloc( (preoutLength+1)* sizeof(thwchar_t));
   *preout = '\0';
 
-  // create the real output
+  /* create the real output */
   for ( i = 0 ; i < tis620_0_outLength; i++) {
-    if ( strchr(cutCode, tis620_0_out[i]) != NULL ) { // ok now we found the cut point
-    //if ( cutCode[0] == tis620_0_out[i] ) { // ok now we found the cut point
-      // change to user supplied cutcode
+    if ( strchr(cutCode, tis620_0_out[i]) != NULL ) { /* ok now we found the cut point */
+    /*if ( cutCode[0] == tis620_0_out[i] ) { // ok now we found the cut point */
+      /* change to user supplied cutcode */
       wcscat( preout, _cutCode);
     } else {
-      // copy to output
+      /* copy to output */
       wcsncat( preout, uni_out + i, 1 );
     }
   };
 
-  // get the real output length
+  /* get the real output length */
   returnValue = wcslen(preout);
 
-  // now copy to the real _out that will be returned to caller
+  /* now copy to the real _out that will be returned to caller */
   if ( returnValue < n ) {
     minValue = returnValue;
   } else {
@@ -104,7 +104,7 @@ int th_wbrk_line(const thwchar_t *in, thwchar_t *_out, size_t n, const thwchar_t
 
   wcsncpy(_out, preout, minValue);
 
-  // Deallocate memory
+  /* Deallocate memory */
   free(uni_out);
   free(preout);
   free(tis620_0_out);
