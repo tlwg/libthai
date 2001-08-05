b742112598456c5ae70506503586e4c86c86a03b
thep <theppitak@gmail.com>
2001-08-05 12:42:16 +0000

    Change th_validate() to take more context. Define conversion mechanism more deliberately. Implement th_validate().

--- include/thai/thinp.h
+++ include/thai/thinp.h
@@ -1,5 +1,5 @@
 /*
- * $Id: thinp.h,v 1.5 2001-08-04 13:04:49 thep Exp $
+ * $Id: thinp.h,v 1.6 2001-08-05 12:42:16 thep Exp $
  * thinp.h - Thai string input sequence filtering
  * Created: 2001-05-17
  */
@@ -27,13 +27,17 @@ typedef enum {
 extern int th_isaccept(thchar_t c1, thchar_t c2, thstrict_t s);
 
 /*
- * Is *c2 allowed to be placed after *c1? And if not, and if swapping
- * them makes it valid, do it.
- * Returns: 1 if (*c1, *c2) is a valid sequence in the first place
- *          2 if (*c1, *c2) has been swapped and the sequence becomes valid
- *          0 if (*c1, *c2) is invalid, no matter how
+ * Given the context c_2 at position (n-2), and c_1 at position (n-1),
+ * consider if c could be placed at position n or if any conversion
+ * can make it fit otherwise.
+ * Returns: 1 if (c_2,c_1,c) is a valid sequence in the first place
+ *          2 if (c_1, c) can be converted to become valid, in which case
+ *            the converted string meaning to replace (c_1, c) is written
+ *            in conv[]
+ *          0 if (c_1, c) CANNOT be validated, no matter how
  */
-extern int th_validate(thchar_t *c1, thchar_t *c2, thstrict_t s);
+extern int th_validate(thchar_t c_2, thchar_t c_1, thchar_t c,
+                       thchar_t conv[3], thstrict_t s);
 
 END_CDECL
 
--- src/thinp/thinp.c
+++ src/thinp/thinp.c
@@ -1,5 +1,5 @@
 /*
- * $Id: thinp.c,v 1.1 2001-08-04 13:04:49 thep Exp $
+ * $Id: thinp.c,v 1.2 2001-08-05 12:42:16 thep Exp $
  * thinp.c - Thai string input sequence filtering
  * Created: 2001-08-04
  */
@@ -29,9 +29,47 @@ int th_isaccept(thchar_t c1, thchar_t c2, thstrict_t s)
     }
 }
 
-int th_validate(thchar_t *c1, thchar_t *c2, thstrict_t s)
+static const struct correction_t {
+    char c1, c2, to[3];
+} corrections[] = {
+    { 'à', 'à', "á" },
+    { 'Ä', 'Ò', "Äå" },
+    { 'Æ', 'Ò', "Æå" },
+    { 0xed, 'Ò', "Ó" },
+    { 0, 0, 0 }
+};
+
+static int correct(thchar_t c_1, thchar_t c, thchar_t conv[3])
 {
-    /* FIXME: implement it! */
+    const struct correction_t *p;
+    for (p = corrections; p->c1; ++p) {
+        if (c_1 == (thchar_t)p->c1 && c == (thchar_t)p->c2) {
+            strcpy((char *)conv, p->to);
+            return 2;
+        }
+    }
+    return 0;
+}
+
+int th_validate(thchar_t c_2, thchar_t c_1, thchar_t c,
+                thchar_t conv[3], thstrict_t s)
+{
+    /* try predefined corrections */
+    int ret = correct(c_1, c, conv);
+    if (ret) return ret;
+
+    if (th_isaccept(c_1, c, s)) return 1;  /* no conversion needed */
+
+    /* try correction by conversion */
+    if (th_isaccept(c_2, c, s)) {
+        *conv++ = c;
+        if (th_isaccept(c, c_1, s)) {
+            *conv++ = c_1;
+        }
+        *conv = '\0';
+        return 2;
+    }
+
     return 0;
 }
 
