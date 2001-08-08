83db4441a4a0864ad62cbe60c603ba51db28ae29
thep <thep@07894a72-abb6-46fe-b0a1-707444d65a2c>
2001-08-08 12:39:19 +0000

    Split thcell functions into a separate module.

---
+++ trunk/include/thai/thcell.h
@@ -0,0 +1,49 @@
+/*
+ * $Id: thcell.h,v 1.1 2001-08-08 12:39:18 thep Exp $
+ * thcell.h - Thai string cell custering
+ * Created: 2001-08-08 (split from thrend.h)
+ */
+
+#ifndef THAI_THCELL_H
+#define THAI_THCELL_H
+
+#include <thai/thailib.h>
+
+BEGIN_CDECL
+
+/* Thai char cell representation */
+struct thcell {
+    thchar_t base;
+    thchar_t hilo;
+    thchar_t top;
+};
+
+/*
+ * get first cell from the string bounded by s and len
+ * Paramater:
+ *   - is_decomp_am determines if SARA_AM is to be decomposed into
+ *     NIKHAHIT and SARA_AA
+ * On return: *cell = the cell; *nchars = total chars consumed by the cell
+ * Returns: pointer to the position next to the cell
+ */
+extern const thchar_t *th_next_cell(const thchar_t *s, size_t len,
+                                    struct thcell *cell, size_t *nchars,
+                                    int is_decomp_am);
+/*
+ * tokenizes the string bounded by s and len into cells
+ * No more than *ncells will be stored in (*cells)[]
+ * Paramater:
+ *   - is_decomp_am determines if SARA_AM is to be decomposed into
+ *     NIKHAHIT and SARA_AA
+ * On return: cells[] = the cells;
+ *            *ncells = total cells stored in (*cells)[]
+ * Returns: pointer to the position next to the last cell stored in (*cells)[]
+ */
+extern const thchar_t *th_make_cells(const thchar_t *s, size_t len,
+                                     struct thcell cells[], size_t *ncells,
+                                     int is_decomp_am);
+
+END_CDECL
+
+#endif  /* THAI_THCELL_H */
+
--- trunk/include/thai/thrend.h
+++ trunk/include/thai/thrend.h
@@ -1,5 +1,5 @@
 /*
- * $Id: thrend.h,v 1.5 2001-08-07 12:40:15 thep Exp $
+ * $Id: thrend.h,v 1.6 2001-08-08 12:39:19 thep Exp $
  * thrend.h - Thai string rendering
  * Created: 2001-05-17
  */
@@ -8,43 +8,12 @@
 #define THAI_THREND_H
 
 #include <thai/thailib.h>
+#include <thai/thcell.h>
 
 BEGIN_CDECL
 
-/* Thai char cell representation */
-struct thcell {
-    thchar_t base;
-    thchar_t hilo;
-    thchar_t top;
-};
-
 /* Glyph code type */
 typedef unsigned int thglyph_t;
-
-/*
- * get first cell from the string bounded by s and len
- * Paramater:
- *   - is_decomp_am determines if SARA_AM is to be decomposed into
- *     NIKHAHIT and SARA_AA
- * On return: *cell = the cell; *nchars = total chars consumed by the cell
- * Returns: pointer to the position next to the cell
- */
-extern const thchar_t *th_next_cell(const thchar_t *s, size_t len,
-                                    struct thcell *cell, size_t *nchars,
-                                    int is_decomp_am);
-/*
- * tokenizes the string bounded by s and len into cells
- * No more than *ncells will be stored in (*cells)[]
- * Paramater:
- *   - is_decomp_am determines if SARA_AM is to be decomposed into
- *     NIKHAHIT and SARA_AA
- * On return: cells[] = the cells;
- *            *ncells = total cells stored in (*cells)[]
- * Returns: pointer to the position next to the last cell stored in (*cells)[]
- */
-extern const thchar_t *th_make_cells(const thchar_t *s, size_t len,
-                                     struct thcell cells[], size_t *ncells,
-                                     int is_decomp_am);
 
 /*
  * render the given cell using corresponding glyph map
