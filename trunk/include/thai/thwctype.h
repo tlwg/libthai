9c2f40cb125bcf7c9c5b08065004d5c60c8aabb7
thep <thep@07894a72-abb6-46fe-b0a1-707444d65a2c>
2001-09-14 14:37:14 +0000

    Add a quick implementation of thwctype.

--- trunk/include/thai/thwctype.h
+++ trunk/include/thai/thwctype.h
@@ -1,5 +1,5 @@
 /*
- * $Id: thwctype.h,v 1.2 2001-05-17 17:58:56 thep Exp $
+ * $Id: thwctype.h,v 1.3 2001-09-14 14:36:04 thep Exp $
  * thwctype.h - Thai wide-char character classifications
  * Created: 2001-05-17
  */
@@ -8,28 +8,27 @@
 #define THAI_THWCTYPE_H
 
 #include <thai/thailib.h>
+#include <thai/thwchar.h>
 
 BEGIN_CDECL
 
-extern int th_wcistis(thwchar_t c);
-extern int th_wciswinthai(thwchar_t c);
-extern int th_wcismacthai(thwchar_t c);
+extern int th_wcistis(thwchar_t wc);
 
-extern int th_wcisthai(thwchar_t c);
-extern int th_wciseng(thwchar_t c);
+extern int th_wcisthai(thwchar_t wc);
+extern int th_wciseng(thwchar_t wc);
 
 /* Thai letter classification */
-extern int th_wcisthcons(thwchar_t c);
-extern int th_wcisthvowel(thwchar_t c);
-extern int th_wcisthtone(thwchar_t c);
-extern int th_wcisthdigit(thwchar_t c);
-extern int th_wcisthpunct(thwchar_t c);
+extern int th_wcisthcons(thwchar_t wc);
+extern int th_wcisthvowel(thwchar_t wc);
+extern int th_wcisthtone(thwchar_t wc);
+extern int th_wcisthdigit(thwchar_t wc);
+extern int th_wcisthpunct(thwchar_t wc);
 
 /* Thai vowel classification */
-extern int th_wcisldvowel(thwchar_t c);
-extern int th_wclsflvowel(thwchar_t c);
-extern int th_wcisupvowel(thwchar_t c);
-extern int th_wcisblvowel(thwchar_t c);
+extern int th_wcisldvowel(thwchar_t wc);
+extern int th_wcisflvowel(thwchar_t wc);
+extern int th_wcisupvowel(thwchar_t wc);
+extern int th_wcisblvowel(thwchar_t wc);
 
 /*
  * For rendering :
@@ -38,7 +37,33 @@ extern int th_wcisblvowel(thwchar_t c);
  *   0 = base
  *  -1 = below
  */
-extern int th_wcchlevel(thwchar_t c);
+extern int th_wcchlevel(thwchar_t wc);
+
+
+/*
+ * implementation parts
+ */
+#include <thai/thctype.h>
+
+#define th_wcistis(wc)      th_istis(th_uni2tis(wc))
+
+#define th_wcisthai(wc)     th_isthai(th_uni2tis(wc))
+#define th_wciseng(wc)      th_iseng(th_uni2tis(wc))
+
+/* Thai letter classification */
+#define th_wcisthcons(wc)   th_isthcons(th_uni2tis(wc))
+#define th_wcisthvowel(wc)  th_isthvowel(th_uni2tis(wc))
+#define th_wcisthtone(wc)   th_isthtone(th_uni2tis(wc))
+#define th_wcisthdigit(wc)  th_isthdigit(th_uni2tis(wc))
+#define th_wcisthpunct(wc)  th_isthpunct(th_uni2tis(wc))
+
+/* Thai vowel classification */
+#define th_wcisldvowel(wc)  th_isldvowel(th_uni2tis(wc))
+#define th_wcisflvowel(wc)  th_isflvowel(th_uni2tis(wc))
+#define th_wcisupvowel(wc)  th_isupvowel(th_uni2tis(wc))
+#define th_wcisblvowel(wc)  th_isblvowel(th_uni2tis(wc))
+
+#define th_wcchlevel(wc)    th_chlevel(th_uni2tis(wc))
 
 END_CDECL
 
