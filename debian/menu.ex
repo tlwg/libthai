835fde61e8477687f209a89a30a08d9ff28fd71c
ott <pattara@gmail.com>
2003-02-02 08:35:56 +0000

    -let's debianize it

---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(libthai):needs=X11|text|vc|wm section=Apps/see-menu-manual\
+  title="libthai" command="/usr/bin/libthai"
---
+++ debian/shlibs.local.ex
@@ -0,0 +1,1 @@
+liblibthai 0.1.1 libthai (>> 0.1.1-0), libthai (<< 0.1.1-99)
