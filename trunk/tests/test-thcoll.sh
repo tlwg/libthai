42fe200523fdd283ec54d9b553f97a5961988dc1
thep <thep@07894a72-abb6-46fe-b0a1-707444d65a2c>
2003-09-10 05:29:46 +0000

    Clean up things for releasing version 0.1.2.
    *** Stop using auto-generated ChangeLog, as it's hard to read ***

--- trunk/tests/test-thcoll.sh
+++ trunk/tests/test-thcoll.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-# $Id: test-thcoll.sh,v 1.2 2001-06-12 13:14:23 thep Exp $
+# $Id: test-thcoll.sh,v 1.3 2003-09-10 05:29:46 thep Exp $
 
 srcdir=${srcdir-.}
 top_builddir=${top_builddir-.}
 
 $top_builddir/tests/thsort $srcdir/sorttest.txt out.txt
-cmp $srcdir/sorted.txt out.txt
+cmp $srcdir/sorted.txt out.txt && rm -f out.txt
 
