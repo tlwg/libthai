f32938bd2be183a7fdfb423920f1f662f2b2234d
thep <theppitak@gmail.com>
2006-09-03 01:41:28 +0000

    2006-09-03  Theppitak Karoonboonyanan  <thep@linux.thai.net>
    	* src/thbrk/thbrk.c (brk_do): (Optimization) In recovery mode, stop
    	immediately when first solution is found.

--- src/thbrk/thbrk.c
+++ src/thbrk/thbrk.c
@@ -321,8 +321,11 @@ brk_do (const thchar_t *s, int len, int pos[], size_t n, int do_recover)
         if (!is_keep_node ||
             node->shot.str_pos == len || node->shot.cur_brk_pos >= n)
         {
-            /* path is done; contest and remove */
-            best_brk_contest (best_brk, &node->shot);
+            /* path is done; contest and remove
+             * if in recovery mode, stop as soon as first solution is found
+             */
+            if (best_brk_contest (best_brk, &node->shot) && !do_recover)
+                break;
             pool = brk_pool_delete (pool, node);
         } else {
             /* find matched nodes, contest and keep the best one */
