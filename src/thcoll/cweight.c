c85751e92e91bb939b684464aa3786f98b086c78
thep-svn <theppitak@gmail.com>
2010-01-30 01:34:15 +0000

    * src/thcoll/cweight.c, src/thcoll/cweight.h, src/thcoll/thcoll.c: 	Remove GPL header left over. The library is LGPL.

--- src/thcoll/cweight.c
+++ src/thcoll/cweight.c
@@ -1,30 +1,4 @@
 /*
-   Copyright (C) 1998 National Electronics and Computer Technology Center
-
-   This file is part of ThColl, a Thai string collation library.
-
-   ThColl is free software; you can redistribute it and/or
-   modify it under the terms of the GNU General Public License as
-   published by the Free Software Foundation; either version 2 of
-   the License, or (at your option) any later version.
-
-   ThColl is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with ThColl; see the file COPYING.  If not, write to the
-   Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-   Boston, MA  02111-1307, USA.
-
-   Any licensing or usage questions should be directed to Theppitak
-   Karoonboonyanan <thep@linux.thai.net>.
-
-*/
-
-
-/*
  * $Id: cweight.c,v 1.5 2007-10-18 14:52:29 thep Exp $
  * cweight.c - Thai Character Weight Table for TIS-620/2533 Collating
  * Created: 9 Nov 1998
