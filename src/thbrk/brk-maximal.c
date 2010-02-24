1837397dc2e034084431de100d306d019ae59e42
thep-svn <theppitak@gmail.com>
2010-02-24 11:31:20 +0000

    Replace <thai/tis.h> with a rewritten one, so we can enforce the 	license.
    	* include/thai/tis.h:
    	Rewritten based on Unicode names, with TIS_ prefix.
    
    	* src/thinp/thinp.c (corrections, th_validate),
    	* src/thrend/thrend.c (shiftdown_tone_ad, shiftdownleft_tone_ad,
    	  shiftleft_tone_ad, shiftdown_bv_bd, tailcutcons, th_render_cell_,
    	  th_render_cell_tis),
    	* src/thbrk/brk-maximal.c (th_brkpos_hints),
    	* src/thcell/thcell.c (th_next_cell, th_prev_cell),
    	* tests/test_thcell.c (test_ans_nodecomp_am, test_ans_decomp_am):
    	Update character names to the new TIS_* ones.

--- src/thbrk/brk-maximal.c
+++ src/thbrk/brk-maximal.c
@@ -128,30 +128,33 @@ th_brkpos_hints (const thchar_t *str, int len, char *hints)
 
     for (i = 0; i < len; /* nop */) {
         if (th_isthcons (str[i])) {
-            if (i+1 < len && str[i+1] == THANTHAKHAT) {
+            if (i+1 < len && str[i+1] == TIS_THANTHAKHAT) {
                 i += 2; /* the cons + THANTHAKHAT */
-            } else if (i+2 < len && str[i+2] == THANTHAKHAT) {
+            } else if (i+2 < len && str[i+2] == TIS_THANTHAKHAT) {
                 i += 3; /* the cons + intermediate char + THANTHAKHAT */
-            } else if (i+2 < len && str[i] != KOKAI && str[i+1] == MAITAIKHU
-                       && (str[i+2] == OANG || str[i+2] == WOWAEN))
+            } else if (i+2 < len
+                       && str[i] != TIS_KO_KAI && str[i+1] == TIS_MAITAIKHU
+                       && (str[i+2] == TIS_O_ANG || str[i+2] == TIS_WO_WAEN))
             {
                 hints[i] = 1;
                 i += 4; /* the cons + MAITAIKHU + OANG/WOWAEN + cons */
             } else if ((i > 0
-                        && (str[i-1] == MAIHUNAKAT || str[i-1] == SARA_UEE))
+                        && (str[i-1] == TIS_MAI_HAN_AKAT
+                            || str[i-1] == TIS_SARA_UEE))
                        || (i > 1 && th_isthtone (str[i-1])
-                           && (str[i-2] == MAIHUNAKAT || str[i-2] == SARA_UEE)))
+                           && (str[i-2] == TIS_MAI_HAN_AKAT
+                               || str[i-2] == TIS_SARA_UEE)))
             {
                 i++;
             } else {
                 hints[i++] = 1;
             }
-        } else if (str[i] == SARA_E || str[i] == SARA_AE) {
+        } else if (str[i] == TIS_SARA_E || str[i] == TIS_SARA_AE) {
             hints[i] = 1; /* sara e/ae */
             i += 2; /* sara e/ae + the supposedly cons */
             if (i >= len)
                 break;
-            if (str[i] == MAITAIKHU) {
+            if (str[i] == TIS_MAITAIKHU) {
                 i += 2; /* MAITAIKHU + the supposedly cons */
             } else if (th_isupvowel (str[i])) {
                 i++; /* the upper vowel, as part of composite vowel */
@@ -159,9 +162,11 @@ th_brkpos_hints (const thchar_t *str, int len, char *hints)
                     i++;
                 i++; /* the supposedly cons */
             } else if (i+2 < len
-                       && ((str[i+1] == SARA_AA && str[i+2] == SARA_A)
-                            || (str[i] != KOKAI && str[i+1] == MAITAIKHU
-                                && str[i+2] != OANG && str[i+2] != WOWAEN)))
+                       && ((str[i+1] == TIS_SARA_AA && str[i+2] == TIS_SARA_A)
+                            || (str[i] != TIS_KO_KAI
+                                && str[i+1] == TIS_MAITAIKHU
+                                && str[i+2] != TIS_O_ANG
+                                && str[i+2] != TIS_WO_WAEN)))
             {
                 i += 3; /* 2nd cons + SARA_AA + SARA_A, or
                          * 2nd cons + MAITAIKHU + final cons
@@ -170,7 +175,7 @@ th_brkpos_hints (const thchar_t *str, int len, char *hints)
         } else if (th_isldvowel (str[i])) {
             hints[i] = 1; /* the ldvowel */
             i += 2; /* the ldvowel + the supposedly cons */
-        } else if (str[i] == RU || str[i] == LU) {
+        } else if (str[i] == TIS_RU || str[i] == TIS_LU) {
             hints[i++] = 1;
         } else {
             i++;
--- src/thcell/thcell.c
+++ src/thcell/thcell.c
@@ -68,22 +68,28 @@ size_t th_next_cell(const thchar_t *s, size_t len,
         do {
             switch (th_chlevel(*s)) {
                 case 0:
-                    if (is_decomp_am && *s == SARA_AM) { acell.hilo = *s++; }
-                    else { acell.base = *s++; }
+                    if (is_decomp_am && *s == TIS_SARA_AM) {
+                        acell.hilo = *s++;
+                    } else {
+                        acell.base = *s++;
+                    }
                     break;
                 case -1:
                 case 1: acell.hilo = *s++; break;
                 case 2: acell.top  = *s++; break;
                 case 3:
-                    if (!acell.hilo) { acell.hilo = *s++; }
-		    else { acell.top = *s++; }
+                    if (!acell.hilo) {
+                        acell.hilo = *s++;
+                    } else {
+                        acell.top = *s++;
+                    }
 		    break;
             }
             ++n; --len;
         } while (
             len > 0 && (
                 is_composible(s[-1], s[0]) || (
-                    is_decomp_am && s[0] == SARA_AM
+                    is_decomp_am && s[0] == TIS_SARA_AM
                     && th_isthcons(acell.base) && acell.hilo == 0
                 )
             )
@@ -118,8 +124,11 @@ size_t th_prev_cell(const thchar_t *s, size_t pos,
             thchar_t c = s[pos-1];
             switch (th_chlevel(c)) {
                 case 0:
-                    if (is_decomp_am && c == SARA_AM) { acell.hilo = c; }
-                    else { acell.base = c; }
+                    if (is_decomp_am && c == TIS_SARA_AM) {
+                        acell.hilo = c;
+                    } else {
+                        acell.base = c;
+                    }
                     break;
                 case 1: 
                     if (acell.hilo && th_chlevel(acell.hilo) == 3) {
@@ -129,15 +138,18 @@ size_t th_prev_cell(const thchar_t *s, size_t pos,
                 case -1: acell.hilo = c; break;
                 case 2: acell.top  = c; break;
                 case 3:
-                    if (!acell.hilo) { acell.hilo = c; }
-		    else { acell.top = c; }
+                    if (!acell.hilo) {
+                        acell.hilo = c;
+                    } else {
+                        acell.top = c;
+                    }
 		    break;
             }
             ++n; --pos;
         } while (
             pos > 0 && (
                 is_composible(s[pos-1], s[pos]) || (
-                    acell.hilo == SARA_AM &&
+                    acell.hilo == TIS_SARA_AM &&
                     !acell.base && (
                         (!acell.top && th_isthtone(s[pos-1])) ||
                         th_isthcons(s[pos-1])
