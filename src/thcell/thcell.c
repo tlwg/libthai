128d3ead1965f0b2f22fefda26cf2d110e52689a
thep-svn <theppitak@gmail.com>
2010-02-24 10:09:06 +0000

    * include/thai/*.h, src/*.c, src/*/*.c: Add license header to every 	source file.

--- src/thcell/thcell.c
+++ src/thcell/thcell.c
@@ -1,4 +1,23 @@
 /*
+ * libthai - Thai Language Support Library
+ * Copyright (C) 2001  Theppitak Karoonboonyanan
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+/*
  * $Id: thcell.c,v 1.10 2007-10-18 14:52:29 thep Exp $
  * thcell_t.c - Thai string cell custering
  * Created: 2001-08-08 (split from thrend.c)
