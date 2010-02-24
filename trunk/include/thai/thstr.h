13c26a992680a14ef2d70f94114af1ca9aadd493
thep-svn <thep-svn@07894a72-abb6-46fe-b0a1-707444d65a2c>
2010-02-24 10:09:06 +0000

    * include/thai/*.h, src/*.c, src/*/*.c: Add license header to every 	source file.

--- trunk/include/thai/thstr.h
+++ trunk/include/thai/thstr.h
@@ -1,4 +1,23 @@
 /*
+ * libthai - Thai Language Support Library
+ * Copyright (C) 2001  Theppitak Karoonboonyanan
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+/*
  * $Id: thstr.h,v 1.2 2006-07-31 11:07:46 thep Exp $
  * thstr.h - Thai string manipulators
  * Created: 2001-08-03
