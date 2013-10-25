e55a332782aea77e909fd28f8391245591a5b2a9
thep-svn <theppitak@gmail.com>
2013-10-25 05:44:30 +0000

    Fix warnings in tests.
    * tests/test_thwchar.c (main):
      - Cast (thchar_t *) to (char *) to fix signedness differences.
      - Cast (thwchar_t) to (unsigned long) for "%lx" string format.
      - Cast (size_t) return values from wcslen() and strlen() to long,
        and use "%ld" format to print them.
      - Replace 'outputLength' var with 'tisLength' and 'uniLength'
        and do some useful checks on return values of th_tis2uni_line()
        and th_uni2tis_line().

--- tests/test_thwchar.c
+++ tests/test_thwchar.c
@@ -16,27 +16,41 @@ int main (int argc, char* argv[])
   thchar_t  tis_620_0[MAXLINELENGTH];
   thchar_t  newtis_620_0[MAXLINELENGTH];
   thwchar_t uni[MAXLINELENGTH];
-  int       outputLength;
+  int       tisLength, uniLength;
   const thchar_t *pc;
   int       err = 0;
 
-  strcpy(tis_620_0,"สวัสดีครับ นี่เป็นการทดสอบตัวเอง");
+  strcpy((char *)tis_620_0,"สวัสดีครับ นี่เป็นการทดสอบตัวเอง");
   
   fprintf(stderr, "Testing thwchar...\n");
+  tisLength = strlen((const char *)tis_620_0);
   fprintf(stderr, "Input:  tis-620-0 string of length %d: %s\n",
-          strlen(tis_620_0), tis_620_0);
+          tisLength, tis_620_0);
 
   /* tis-620-0 to Unicode conversion */
-  outputLength = th_tis2uni_line(tis_620_0, uni, MAXLINELENGTH);
-  fprintf(stderr, "Output: Unicode string of length %d:", wcslen(uni));
+  uniLength = th_tis2uni_line(tis_620_0, uni, MAXLINELENGTH);
+  fprintf(stderr, "Output: Unicode string of length %d, wcslen = %ld\n",
+          uniLength, (long)wcslen(uni));
+  if (uniLength != tisLength) {
+    fprintf (stderr,
+             "th_tis2uni_line() returns different length %d from original %d\n",
+             uniLength, tisLength);
+    err = 1;
+  }
 
   /* Unicode to tis-620-0 conversion */
   fprintf(stderr, "\nConvert back to tis-620-0 string...\n");
-  outputLength = th_uni2tis_line(uni, newtis_620_0, MAXLINELENGTH);
-  fprintf(stderr, "Output: tis-620-0 string of length %d: %s\n",
-          strlen(newtis_620_0), newtis_620_0);
+  tisLength = th_uni2tis_line(uni, newtis_620_0, MAXLINELENGTH);
+  fprintf(stderr, "Output: tis-620-0 string of length %d, strlen = %ld: %s\n",
+          tisLength, (long)strlen((const char *)newtis_620_0), newtis_620_0);
+  if (tisLength != uniLength) {
+    fprintf (stderr,
+             "th_uni2tis_line() returns different length %d from original %d\n",
+             tisLength, uniLength);
+    err = 1;
+  }
 
-  if (strcmp(newtis_620_0, tis_620_0) == 0) {
+  if (strcmp((const char *)newtis_620_0, (const char *)tis_620_0) == 0) {
     fprintf(stderr, " Input = output, correct! Test thwchar OK.\n");
   } else {
     fprintf(stderr, " Input != output, incorrect!!\n");
@@ -47,14 +61,16 @@ int main (int argc, char* argv[])
   for (pc = win_sample; *pc; ++pc) {
     if (th_uni2winthai(th_winthai2uni(*pc)) != *pc) {
       fprintf(stderr, "Inconsistent uni<->winthai conv: %02x -> %04lx, %02x\n",
-              *pc, th_winthai2uni(*pc), th_uni2winthai(th_winthai2uni(*pc)));
+              *pc, (unsigned long)th_winthai2uni(*pc),
+              th_uni2winthai(th_winthai2uni(*pc)));
       err = 1;
     }
   }
   for (pc = mac_sample; *pc; ++pc) {
     if (th_uni2macthai(th_macthai2uni(*pc)) != *pc) {
       fprintf(stderr, "Inconsistent uni<->macthai conv: %02x -> %04lx, %02x\n",
-              *pc, th_macthai2uni(*pc), th_uni2macthai(th_macthai2uni(*pc)));
+              *pc, (unsigned long)th_macthai2uni(*pc),
+              th_uni2macthai(th_macthai2uni(*pc)));
       err = 1;
     }
   }
